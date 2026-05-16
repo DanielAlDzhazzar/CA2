c23d6971dfe8a27a7c17ce10b952855c4a0bee06
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 22:38:30 +0100

    feat: add public MiniSearchEngine() as a constructor for MiniSearchEngine

--- src/engine/MiniSearchEngine.java
+++ src/engine/MiniSearchEngine.java
@@ -11,4 +11,25 @@ import java.util.Scanner;
  */
 
 public class MiniSearchEngine {
+    private HashMap<String, LinkedList<String>> invertedIndex;
+
+    private String[] files = {
+            "data/file1.txt",
+            "data/file2.txt",
+            "data/file3.txt",
+            "data/file4.txt",
+            "data/file5.txt",
+            "data/file6.txt",
+            "data/file7.txt",
+            "data/file8.txt",
+            "data/file9.txt",
+            "data/file10.txt"
+    };
+
+    public MiniSearchEngine(){
+        invertedIndex = new HashMap<>();
+        buildIndex();
+    }
+
+
 }
