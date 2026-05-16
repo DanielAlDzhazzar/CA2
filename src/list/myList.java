0f0cbc6d06a41526cef7f366cb6c51c75ad20d33
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 19:43:52 +0100

    feat: created an Interface for list

--- src/list/myList.java
+++ src/list/myList.java
@@ -1,4 +1,11 @@
 package list;
 
-public class myList {
+public interface myList<T> {
+    void add(T value);
+    T get(int index);
+    boolean contains(T value);
+    int size();
+    boolean isEmpty();
+    T removeLast();
+    void clear();
 }
