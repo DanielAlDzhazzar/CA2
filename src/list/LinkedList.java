e4a0adf47b82c222ee31e0a0733602b32b88802a
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 20:03:21 +0100

    docs: added JavaDoc for public class LinkedList<T>

--- src/list/LinkedList.java
+++ src/list/LinkedList.java
@@ -1,5 +1,11 @@
 package list;
 
+/**
+ * A singly-linked LinkedList, using both first and last nodes
+ *
+ * @param <T> type of data to be stored in a list
+ */
+
 public class LinkedList<T> implements MyList<T>{
     private Node<T> head;
     private Node<T> tail;
