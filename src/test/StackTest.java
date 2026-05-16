d077713275e52e4f31021485839f604e2bd755d0
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 23:39:48 +0100

    feat: add public void testLIFOOrder() to test Stack LIFO compliance

--- src/test/StackTest.java
+++ src/test/StackTest.java
@@ -76,4 +76,17 @@ public class StackTest {
 
         assertNull(stack.peek());
     }
+
+    @Test
+    public void testLIFOOrder(){
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals(3, stack.pop());
+        assertEquals(2, stack.pop());
+        assertEquals(1, stack.pop());
+    }
 }
