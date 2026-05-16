33613f36dcf7afe82417b9728461d2dac90dfda5
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 22:24:21 +0100

    feat: add public static String readFile(String filename) to read text from a file

--- src/engine/FileProcessor.java
+++ src/engine/FileProcessor.java
@@ -1,6 +1,7 @@
 package engine;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,4 +10,23 @@ import java.io.IOException;
  */
 
 public class FileProcessor {
+    public static String readFile(String filename){
+        StringBuilder builder = new StringBuilder();
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+            String line;
+
+            while((line = reader.readLine()) != null){
+                builder.append(line).append(" ");
+            }
+
+            reader.close();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        return builder.toString();
+    }
 }
