097e631a2fd536900f01ef2dab7f3017741c8739
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 21:33:55 +0100

    feat: add private static class Entry<K, V> for HashMap key-value storage

--- src/hashmap/HashMap.java
+++ src/hashmap/HashMap.java
@@ -10,4 +10,14 @@ import list.LinkedList;
  */
 
 public class HashMap <K, V>{
+    private static class Entry<K, V>{
+        K key;
+        V value;
+        Entry<K, V> next;
+
+        Entry(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
