f370a7d2e98a34dcd12f8606d49dd775774e01df
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 23:19:59 +0100

    fix: moved user interaction from this file to Main

--- src/engine/MiniSearchEngine.java
+++ src/engine/MiniSearchEngine.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 
 public class MiniSearchEngine {
-    private static HashMap<String, LinkedList<String>> invertedIndex;
+    private HashMap<String, LinkedList<String>> invertedIndex;
 
     private String[] files = {
             "data/file1.txt",
@@ -59,61 +59,6 @@ public class MiniSearchEngine {
                 else if(!fileList.contains(file)){
                     fileList.add(file);
                 }
-            }
-        }
-    }
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        MiniSearchEngine engine = new MiniSearchEngine();
-
-        while (true){
-            System.out.println("1. Search for a word");
-            System.out.println("2. Display all indexed words");
-            System.out.println("3. Exit");
-            System.out.print("Choose option: ");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice){
-                case 1:
-                    System.out.print("Enter the words to search: ");
-                    String word = scanner.nextLine().toLowerCase();
-
-                    LinkedList<String> results = engine.invertedIndex.get(word);
-
-                    if(results == null || results.isEmpty()){
-                        throw new IllegalArgumentException("Word not found.");
-                    }
-
-                    System.out.println("Found in: ");
-                    for(int i = 0; i < results.size(); i++){
-                        System.out.println(results.get(i));
-                    }
-                    break;
-                case 2:
-                    Object[] keys = invertedIndex.getKeys();
-                    String[] words = new String[keys.length];
-
-                    for (int i = 0; i < keys.length; i++){
-                        words[i] = (String) keys[i];
-                    }
-
-                    Arrays.sort(words);
-
-                    System.out.println("\nIndexed Words:");
-
-                    for (String w : words){
-                        System.out.println(w);
-                    }
-                    break;
-                case 3:
-                    System.out.println("Exiting application...");
-                    return;
-                default:
-                    System.out.println("Invalid option.");
             }
         }
     }
