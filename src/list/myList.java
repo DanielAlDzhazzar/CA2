a9e0cb924475a69d729f21ae6e567156377e10b3
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2026-05-16 19:46:36 +0100

    docs: add JavaDoc to myList

--- src/list/myList.java
+++ src/list/myList.java
@@ -1,5 +1,11 @@
 package list;
 
+/**
+ * An interface for a list
+ *
+ * @param <T> type of data stored in a list
+ */
+
 public interface myList<T> {
     void add(T value);
     T get(int index);
